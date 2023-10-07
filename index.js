73270de0eb5831dee2bd8ebae16dab10e578476f
Timur <maksi_jn24@itstep.edu.az>
2023-10-07 12:29:38 +0400

    Difficulty 4 auth

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
-// Level 3
+// Level 4
 
-alert("You need to register");
+register: while (true) {
+    alert("You need to register");
 
-const userLogin = String(prompt("Enter new login"));
-const userPassword = String(prompt("Enter password"));
+    const userLogin = String(prompt("Enter new login"));
+    const userPassword = String(prompt("Enter password"));
 
-alert("You need to login");
+    login: while (true) {
+        let attempts = 3;
 
-let tempLogin = String(prompt("Enter your login"));
-let tempPassword = String(prompt("Enter your password"));
-let success = true;
+        alert("You need to login");
 
-if (tempLogin !== userLogin) {
-    success = false;
-}
+        let tempLogin = String(prompt("Enter your login"));
+        let tempPassword = String(prompt("Enter your password"));
+        let success = true;
 
-if (tempPassword !== userPassword) {
-    success = false;
-}
+        if (tempLogin !== userLogin) {
+            success = false;
+        }
 
-while (success === false) {
-    alert("Incorrect login or password");
-    tempLogin = String(prompt("Enter your login"));
-    tempPassword = String(prompt("Enter your password"));
+        if (tempPassword !== userPassword) {
+            success = false;
+        }
 
-    if (tempLogin === userLogin && tempPassword === userPassword) {
-        success = true;
+        while (!success) {
+            --attempts; // Максимум попыток 3, включая изначальную
+
+            if (attempts > 0) {
+
+                alert(`Incorrect login or password. Attempts: ${attempts}`);
+                tempLogin = String(prompt("Enter your login"));
+                tempPassword = String(prompt("Enter your password"));
+
+                if (tempLogin === userLogin && tempPassword === userPassword) {
+                    success = true;
+                    break login;
+                }
+            } else {
+                if (confirm("Want to create new account?")) {
+                    continue register;
+                } else {
+                    continue login;
+                }
+                
+            }
+        }
     }
-}
 
-alert("Successful auth");
+    alert("Successful auth");
+    break;
+}
