cea6e29707f03335b8f34394ec51035b8c52f129
Timur <maksi_jn24@itstep.edu.az>
2023-10-07 11:54:56 +0400

    Difficult 3 auth

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 alert("You need to register");
 
-const userLogin = String(prompt("Create new login"));
-const userPassword = String(prompt("Create password"));
+const userLogin = String(prompt("Enter new login"));
+const userPassword = String(prompt("Enter password"));
 
 alert("You need to login");
 
 let tempLogin = String(prompt("Enter your login"));
-let tempPassword = String(prompt("Type your password"));
+let tempPassword = String(prompt("Enter your password"));
 let success = true;
 
 if (tempLogin !== userLogin) {
@@ -20,16 +20,13 @@ if (tempPassword !== userPassword) {
 }
 
 while (success === false) {
+    alert("Incorrect login or password");
     tempLogin = String(prompt("Enter your login"));
-    tempPassword = String(prompt("Type your password"));
+    tempPassword = String(prompt("Enter your password"));
 
     if (tempLogin === userLogin && tempPassword === userPassword) {
         success = true;
     }
 }
 
-if (success === true) {
-    alert("Successful auth");
-} else {
-    alert("Incorrect login or password");
-}
+alert("Successful auth");
