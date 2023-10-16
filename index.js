7203409e76d349ce82bbf30eac9499e57a18fdb3
Timur <maksi_jn24@itstep.edu.az>
2023-10-16 15:09:19 +0400

    4. Дни месяца

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-/* 3. Дано действительное неотрицательное число. Написать программу, которая при каждом запуске, будет возвращать случайную
-  строку заданной длины. Строка может состоять, только из букв английского алфавита и цифр */
+/* 4. Пользователь вводит 2 строки: год и месяц. Написать программу, которая вернёт сколько всего дней в указанном месяце */
 
 console.clear();
 
-const alphabet =
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-'a', 'b', 'c', 'd', 'b', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const year = Number(prompt("Enter year"));
+const month = Number(prompt("Enter month number"));
 
-const count = 20;
-const resultString = [];
+const date = new Date(year, month, 0);
 
-for (let i = 0; i < count; ++i) {
-    const randomIndex = Math.round(Math.random() * (alphabet.length - 1));
-    resultString.push(alphabet[randomIndex]);
-}
-
-console.log(resultString.join(''));
+console.log(date.getDate());
