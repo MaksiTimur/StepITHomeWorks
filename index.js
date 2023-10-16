53bb6bdf1998ffe26d43067318a030c9a5dc0dc0
Timur <maksi_jn24@itstep.edu.az>
2023-10-16 13:34:30 +0400

    Второе задание

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-/*1. Даны два массива. Один массив содержит в себе названия глав, а второй массив соответсвующие номера страниц. Количество элементов в обоих массивах одинаково.
-   Нужно вывести в консоль попарно в виде оглавления все названия и соответсвующие им страницы. Минимальная длина всей страницы 20 символов. Если сумма длин
-   номера страницы и названия главы превышает 20 символов, то между ними должна быть 1 точка гарантированно*/
+/* 2. Дан объект. Бесконечно спрашивать у пользователя ввод строки. Введённая строка является названием поля в объекте. Нужно в консоль выводить true или false
+   в зависимости от того, есть ли в объекте поле с таким названием или нет. Сложность заключается в том, что регистр букв не должен учитываться ни в введёной строке
+   ни в названии поля*/
+   
+const obj = {
+    name: 'Alex',
+    surName: 'Max',
+    AGE: 40
+}
 
-console.clear();
+while (true) {
+    let fieldName = prompt("Enter field name");
+    let isFieldExist = false;
 
-{
-    const titles = ['Заголовок 1', 'Самый длинный заголовок во всей книжке', 'Заголовок 2'];
-    const pages = [10, 20, 123456789012345];
-
-    const maxLength = 20;
-
-    for (let i = 0; i < titles.length; ++i) {
-
-        let result;
-        let dotsCount = maxLength - String(pages[i]).length;
-
-        if (titles[i].length >= dotsCount) {
-            result = titles[i].concat('.', String(pages[i]))
-        } else {
-            result = titles[i].padEnd(dotsCount, '.').concat(String(pages[i]));
+    for (const iterator in obj) {
+        if (iterator.toLowerCase() === fieldName.toLowerCase()) { // toLowerCase приводит строку к нижнему регистру
+            isFieldExist = true;
+            break;
         }
-
-        console.log(result);
     }
+
+    console.log(isFieldExist);
 }
