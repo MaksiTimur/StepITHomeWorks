36977f5a6bcf1c9da1a07500d36a4273830219c9
Timur <maksi_jn24@itstep.edu.az>
2023-10-25 12:49:40 +0400

    1. setInterval через setTimeout

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-/* 5. Дано неотрицательное целое число n. Написать функцию, которая на вход принимает данное число и возвращает n-ое число из ряда Фибоначчи */
+/* 1. Реализовать setInterval с помощью setTimeout */
 
 console.clear();
 
-const number = Number(prompt("Enter the number"));
+function customInterval(ms, message) {
+  console.log(message);
 
-let num1 = 0;
-let num2 = 1;
-let sum;
-
-for (let i = 2; i < number; i++) { // Первые два числа уже напечатаны
-    sum = num1 + num2;
-    num1 = num2;
-    num2 = sum;
+  setTimeout(customInterval, ms, ms, message);
 }
 
-console.log(num2);
+customInterval(1000, 'Hi');
