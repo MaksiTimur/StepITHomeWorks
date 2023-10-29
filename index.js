078869ee31da43d84fe3e3518efddecdcb509510
Timur <maksi_jn24@itstep.edu.az>
2023-10-29 16:53:38 +0400

    Добавление метода unshift

--- index.js
+++ index.js
@@ -36,8 +36,6 @@ function MyArray(...args) {
         delete this.elements[0];
         --this.elements.length;
 
-        console.log(this.elements);
-
         const keys = Object.keys(this.elements);
         for (const iterator of keys) {
 
@@ -53,6 +51,22 @@ function MyArray(...args) {
         return returnValue;
     }
 
+    this.unshift = function(...args) {
+
+        const length = this.elements.length;
+
+        for (let i = this.elements.length - 1; i >= 0; --i) {
+            this.elements[i + args.length] = this.elements[i];
+        }
+
+        for (let i = 0; i < args.length; ++i) {
+            this.elements[i] = args[i];
+            ++this.elements.length;
+        }
+
+        return this.elements.length;
+    }
+
     this.push(...args);
 }
 
@@ -60,3 +74,4 @@ const arr = new MyArray(5, 50, 500);
 console.log(arr.elements);
 
 console.log(arr.shift(), arr.elements);
+console.log(arr.unshift(1, 5, 'a'), arr.elements);
