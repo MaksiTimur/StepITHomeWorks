190a2f87cc61030c21928a0eae2c413c1cf7b096
Timur <maksi_jn24@itstep.edu.az>
2023-10-29 15:19:29 +0400

    myMath

--- index.js
+++ index.js
@@ -1,25 +1,74 @@
-/* 4. Написать функцию будильник. Функция должна принимать 1 аргумент:
-	+ Время - строка формата HH:mm:ss
-   Если хоть одна из частей времени не является формата - 2 цифры, то выводить ошибку и ничего не делать.
-   При наступлении указанного времени выводить адекв */
+/* В функции myMath добавить:
+  1. Проверку на то что переданная операция - это функция. Напрмер Math есть константы типа Math.PI и если я щас передам 'PI'
+   как `operation`, то он его найдёт и выведет ошибку, а надо чтобы просто вернул значение Math.PI
+  2. Если передана не существушая операция (ни в Math ни в switch), то вывести ошибку, что такой операции нет
+  3. Написать логику для multiply, division и subtract. Начальные значения 1, 1 и 0 соответсвенно */
 
 console.clear();
 
-let interval;
+console.clear();
 
-function clock(date) {
-  if (date.length !== 8) return("Incorrect time format: hh:mm:ss");
+function myMath(operation, ...args) {
+    const isOperationAlreadyExist = operation in Math;
+    const mathOperation = Math[operation];
 
-  interval = setInterval(checkTime, 1000, date);
+    if (isOperationAlreadyExist) {
+        if (typeof mathOperation === Function) {
+            return mathOperation(...args);
+        } else {
+            return mathOperation;
+        }
+    }
+
+    switch(operation) {
+        case 'sum': {
+            let sum = 0;
+
+            for (let i = 0; i < args.length; ++i) {
+                sum += args[i];
+            }
+
+            return sum;
+        }
+
+        case 'subtract': {
+            let subtract = 0;
+
+            for (let i = 0; i < args.length; ++i) {
+                subtract -= args[i];
+            }
+
+            return subtract;
+        }
+
+        case 'multiply': {
+            let product = 1;
+
+            for (let i = 0; i < args.length; ++i) {
+                product *= args[i];
+            }
+
+            return product;
+        }
+
+        case 'division': {
+            let product = args[0];
+
+            for (let i = 1; i < args.length; ++i) {
+                product /= args[i];
+            }
+
+            return product;
+        }
+
+
+        default: {
+            return "Invalid operation";
+        }
+    }
 }
 
-function checkTime(date) {
-  const time = Date().split(' ')[4];
-
-  if (date === time) {
-    clearInterval(interval);
-    console.log("Будильник!");
-  }
-}
-
-clock("14:54:45");
+console.log(myMath('subtract', 1, 2, 3, 4, 5));
+console.log(myMath('multiply', 2, 4, 2));
+console.log(myMath('division', 4, 2, 2));
+console.log(myMath('PI', -1, 0, 1));
