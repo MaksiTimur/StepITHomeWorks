9bf85137f002294b05f9087a1332a8c9144508ba
Timur <maksi_jn24@itstep.edu.az>
2023-11-13 12:33:53 +0400

    Наследование функций вместо композиции

--- index.js
+++ index.js
@@ -1,15 +1,129 @@
-/* Написать функцию, которая возвращает всю цепочку прототипов для любого объекта вплоть до null. После того как
-показывается null в консоли программа завершает свою работу */
+/* Сделать это задание с помощью наследования классов (Team ни от чего не наследуется, там остаётся композиция) */
 
 console.clear();
 
-function getProtoChain(object) {
-    let currentObj = object;
+function Human(name, age) {
+    const _name = name;
+    const _age = age;
 
-    while (Object.getPrototypeOf(currentObj) !== null) {
-        console.log(Object.getPrototypeOf(currentObj).constructor.name);
-        currentObj = Object.getPrototypeOf(currentObj);
+    Object.defineProperty(this, 'name', {
+        get() {
+            return _name;
+        }
+    });
+
+    Object.defineProperty(this, 'age', {
+        get() {
+            return _age;
+        }
+    });
+}
+
+function Player(name, age, sportName, inMainTeam) {
+    Human.call(name, age);
+
+    const _sportName = sportName;
+    const _inMainTeam = inMainTeam;
+
+    Object.defineProperty(this, 'name', {
+        get() {
+            return name;
+        }
+    });
+
+    Object.defineProperty(this, 'age', {
+        get() {
+            return age;
+        }
+    });
+
+    Object.defineProperty(this, 'sportName', {
+        get() {
+            return _sportName;
+        }
+    });
+
+    Object.defineProperty(this, 'inMainTeam', {
+        get() {
+            return _inMainTeam;
+        }
+    });
+}
+
+Object.setPrototypeOf(Player.prototype, Human.prototype);
+
+function FootbalPlayer(name, age, inMainTeam) {
+    Player.call(name, age, 'footbal', inMainTeam);
+
+    Object.defineProperty(this, 'name', {
+        get() {
+            return name;
+        }
+    });
+
+    Object.defineProperty(this, 'age', {
+        get() {
+            return age;
+        }
+    });
+
+    Object.defineProperty(this, 'sportName', {
+        get() {
+            return sportName;
+        }
+    });
+
+    Object.defineProperty(this, 'inMainTeam', {
+        get() {
+            return inMainTeam;
+        }
+    });
+}
+
+Object.setPrototypeOf(FootbalPlayer.prototype, Player.prototype);
+
+function Team() {
+    const _footbalPlayers = [];
+
+    Object.defineProperty(this, 'players', {
+        get() {
+            return [..._footbalPlayers];
+        }
+    })
+    
+    this.addPlayers = function (...footbaPlayers) {
+        _footbalPlayers.push(...footbaPlayers);
     }
 
-    console.log(Object.getPrototypeOf(currentObj));
+    this.deletePlayer = function (footbaPlayer) {
+        const indexOfFootbalPlayer = _footbalPlayers.indexOf(footbaPlayer);
+
+        if (indexOfFootbalPlayer !== -1) {
+            _footbalPlayers.splice(indexOfFootbalPlayer, 1);
+        }
+    }
+
+    this.getPlayersSortedByName = function () {
+        return [..._footbalPlayers].sort(function (a, b) {
+            const name1 = a.name;
+            const name2 = b.name;
+
+            const name1InLowerCase = name1.toLowerCase();
+            const name2InLowerCase = name2.toLowerCase();
+
+            if (name1InLowerCase > name2InLowerCase) return 1;
+            if (name1InLowerCase < name2InLowerCase) return -1;
+
+            if (name1 > name2) return 1;
+            if (name1 < name2) return -1;
+
+            return 0;
+        })
+    }
+
+    this.getFilteredPlayers = function(inMainTeam) {
+        return _footbalPlayers.filter(function(footballPlayer) {
+            return footballPlayer.inMainTeam === inMainTeam;
+        })
+    }
 }
