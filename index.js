3ea152f980d2a9fab94ea8c999813ed5573459ed
Timur <maksi_jn24@itstep.edu.az>
2023-12-18 12:11:13 +0400

    5. Замена цифр на D

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ console.clear();
 
 // 4. Используя regexp и replace, заменить в строке все цифры на знак D
 
-const regexp = /\d/g;
+const regexp = /\d+/g;
 const string = 'test123 45 lz1g75f'
 
 function replaceByRegExp(str, regexp) {
-    return str.replace(regexp, 'D');
+    return str.replace(regexp, 'N');
 }
 
 console.log(replaceByRegExp(string, regexp));
